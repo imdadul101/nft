c49d8611e286cddb635f26cda6348ef8632e8f76
imdadul101 <imdadul7860@gmail.com>
2022-03-19 10:34:53 +0530

    added

---
+++ basic.js
@@ -0,0 +1,80 @@
+// String
+// Number
+// Boolean
+// undefined
+// null
+// Symbol
+// Objects
+
+// var text = "Hello World 24 can't ${textthree}"
+// var texttwo = 'Hello World 24 ${textthree}'
+// var textthree = `${text}`
+// var textfour = "4"
+// console.log( typeof(text))
+// console.log( typeof(texttwo))
+// console.log( typeof(textthree))
+
+// var n = 12
+// var n2 = 12.5
+
+
+// console.log(typeof n)
+// console.log(typeof n2)
+
+// var a;
+// console.log(typeof a)
+
+// var a=null;
+// console.log(typeof a)
+
+// const user={
+//     username:"imdadul",
+//     password:"imdadul101",
+//     email:"imdadul7860@gmail.com",
+// }
+// console.log(user)
+
+// const laptop={
+//     brand:"hp",
+//     size:"16 inch",
+//     price:"60000",
+//     ram:"4gb",
+//     color:"silver"
+
+// }
+// console.log(laptop)
+
+// const user=[
+//     cont user={    
+//     username:"imdadul",
+//     password:"imdadul101",
+//     email:"imdadul7860@gmail.com",
+//     },
+//     const user={
+//     username:"imdadul",
+//     password:"imdadul101",
+//     email:"imdadul7860@gmail.com",
+//     }
+// ]
+
+// user.find(i=>.email===imdadul7860@gmail.com)********
+
+// const user={
+//         username:"imdadul",
+//         password:"imdadul101",
+//         email:"imdadul7860@gmail.com",
+//     }
+//     console.log(user.username)
+//     console.log(user.password)
+
+var nftcard={
+    nftimage:"url",
+    currency:"ETH",
+    price:"9.04",
+    company:"ofspacenft",
+    likes:"120",
+    id:"nft cube design #92",
+    avatar:"man",
+    title:"@ofspace99",
+    coinicon:"url"
+}
